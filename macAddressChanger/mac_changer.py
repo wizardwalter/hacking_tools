7b023a59fce2e9da702a9b772388c33eb39f9cd6
walter v <waltervannoy@yahoo.com>
2025-01-17 22:36:01 -0600

    testing something

--- macAddressChanger/mac_changer.py
+++ macAddressChanger/mac_changer.py
@@ -5,7 +5,7 @@ import subprocess
 interface = input("interface >") # use raw_input for python2^
 new_mac = input("New Mac Address >")
 
-print("[+] Changing Mac Address for "+ interface + " to " + new_mac)
+print("[+] Changing Mac_Address for "+ interface + " to " + new_mac)
 
 subprocess.call("ifconfig " + interface + " down", shell=True)
 subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
