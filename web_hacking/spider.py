822007d6dd2d5f91f0c928854cd460935a0b2715
walter v <waltervannoy@yahoo.com>
2025-02-21 13:57:09 -0600

    finish web crawler given a target url

--- web_hacking/spider.py
+++ web_hacking/spider.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
-
+from urllib.parse import urljoin
 import requests, re
 
-def request(url):
-    try:
-        return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
-        pass
+target_url = "http://192.168.120.2/mutillidae/"
+tarket_links = []
+def extract_links_from(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"',response.content.decode(errors="ignore"))
 
-target_url = "192.168.120.2/mutillidae/"
-response = requests.get("http://" + target_url)
-href_links = re.findall('(?:href=")(.*?)"',response.content.decode())
-print(href_links)
+def crawl(url):
+    href_links = extract_links_from(url)
+    for link in href_links:
+        link = urljoin(url, link)
+
+        if "#" in link:
+            link = link.split("#")[0]
+
+        if target_url in link and link not in tarket_links:
+            tarket_links.append(link)
+            print(link)
+            crawl(link)
+
+crawl(target_url)
