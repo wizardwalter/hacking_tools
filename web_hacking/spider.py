cf1140e314b897e0a3a69c12a375b93136d3cd1e
walter v <waltervannoy@yahoo.com>
2025-02-20 20:35:20 -0600

    WIP: spider.py

--- web_hacking/spider.py
+++ web_hacking/spider.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import requests, re
+
+def request(url):
+    try:
+        return requests.get("http://" + url)
+    except requests.exceptions.ConnectionError:
+        pass
+
+target_url = "192.168.120.2/mutillidae/"
+response = requests.get("http://" + target_url)
+href_links = re.findall('(?:href=")(.*?)"',response.content.decode())
+print(href_links)
