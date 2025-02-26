0664e5f46eb9ca261e6ce914493f93e5266da62a
walter v <waltervannoy@yahoo.com>
2025-02-25 20:59:01 -0600

    complete simple form extract program

--- extract_forms/extract_forms.py
+++ extract_forms/extract_forms.py
@@ -2,6 +2,8 @@
 
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
 def request(url):
     try:
         return requests.get(url)
@@ -10,6 +12,20 @@ def request(url):
 
 target_url = "http://10.0.0.165/mutillidae/index.php?page=dns-lookup.php"
 response = request(target_url)
-parsed_html = BeautifulSoup(response.content)
+parsed_html = BeautifulSoup(response.content, features="lxml")
 forms_list = parsed_html.findAll("form")
-print(forms_list)
+for form in forms_list:
+    action = form.get("action")
+    post_url = urljoin(target_url, action)
+    method = form.get("method")
+    inputs_list = form.findAll("input")
+    post_data = {}
+    for input in inputs_list:
+        input_name = input.get("name")
+        input_type = input.get("type")
+        input_value = input.get("value")
+        if input_type == 'text':
+             input_value = 'test'
+             post_data[input_name] = input_value
+    result = requests.post(post_url, data = post_data)
+    print(result.content)
